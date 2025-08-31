95e2fad945eb322fb8cf956ca77b77167de0fe25
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2025-08-31 10:33:30 +0700

    Thêm sections trong homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function HomePage() {
-  return <h1>HomePage</h1>;
+    return (
+        <>
+            <section>
+                <h1>Banner</h1>
+            </section>
+            <hr />
+            <section>
+                <h1>Các công ty hàng đầu</h1>
+            </section>
+            <hr />
+            <section>
+                <h1>Các dịch vụ nổi bật từ các hãng taxi</h1>
+            </section>
+            <hr />
+            <section>
+                <h1>Các công việc driver nổi bật từ các hãng taxi</h1>
+            </section>
+            <hr />
+        </>
+    );
 }
 
 export default HomePage;
